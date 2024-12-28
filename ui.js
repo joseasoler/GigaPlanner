8bc97fc48850cb244421e4d80da407387dc7baf2
MultiDyls <110580268+MultiDyls@users.noreply.github.com>
2024-12-28 15:28:02 +1000

    test fix 3
    UI 744 + 752

--- ui.js
+++ ui.js
@@ -741,7 +741,7 @@ function updateCustomSelectOptions(){
 function updateRaceSelect(){
   let raceSel = $("#raceSelect");
   raceSel.empty();
-  for(let i = 0; i < curRaceList.length; i++){
+  for(let i = 0; i < 1; i++){
     raceSel.append(`<option value="${i}">${curRaceList[i].name}</option>`);
   }
 }
@@ -749,7 +749,7 @@ function updateRaceSelect(){
 function updateBlessingSelect(){
   let blessSel = $("#blessingSelect");
   blessSel.empty();
-  for(let i = 0; i < curBlessingList.length; i++){
+  for(let i = 0; i < 1; i++){
 	let name = curBlessingList[i].name
 	if (name.includes("&mdash;")){
 			blessSel.append(`<optgroup label="${name}">`)
